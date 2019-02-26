8bd29a4b70a1a8bd5ec9ce298a68486b4bcc0019
briskcasual <briskcasual@gmail.com>
2019-02-26 15:42:16 -0500

    render type

--- view.js
+++ view.js
@@ -9,6 +9,14 @@ canvas.height = 224;
 
 container.appendChild(canvas);
 
+// render obj type methods
+var renderType = {
+	player: function(cell){
+		ctx.fillStyle = 'green';
+		ctx.fillRect(cell.x*32,cell.y*32,32,32);
+	}
+};
+
 var renderCells = function(){
 	
 	ctx.fillStyle = 'black';
@@ -17,8 +25,13 @@ var renderCells = function(){
 	ctx.strokeStyle = 'white';
 	var i = Game.cells.length,cell;
 	while(i--){
-       cell = Game.cells[i];
-	   ctx.strokeRect(cell.x*32,cell.y*32,32,32);
+        cell = Game.cells[i];
+	    ctx.strokeRect(cell.x*32,cell.y*32,32,32);
+		
+		if(cell.contents.objType){	
+			renderType[cell.contents.objType](cell);
+		}
+		
 	}
 	
 };
