867176f5cacacc2d0154b5737ac7c9fa29129d6e
briskcasual <briskcasual@gmail.com>
2019-02-26 12:38:06 -0500

    CellObj

--- game.js
+++ game.js
@@ -9,20 +9,31 @@ ctx.fillStyle = 'black';
 ctx.fillRect(0,0,canvas.width,canvas.height);
 container.appendChild(canvas);
 
-// GRID
-
-var Grid = (function(){
-
-	// public api returned to grid
+// Game
+var Game = (function(){
+	// public api
 	var api = {
 	    w: 5,
 		h: 5,
 	    cells: []
 	};
 	
-	// set up the grid
+	// DISPOBJ - display object base Class
+	var CellObj = function(){
+	};
+	// default objType is none
+	CellObj.prototype.objType = 'none';
+	
+	// PLAYEROBJ
+	var PlayerCellObj = function(){
+		this.objType = 'player';
+	};
+	PlayerCellObj.prototype = Object.create(CellObj.prototype);
+
+	// set up the game
 	api.setup = function(){
 		var i = this.w * this.h,cell;
+		// create cells
 		this.cells = [];
 		while(i--){
 		    cell = this.cells[i] = {};
@@ -30,15 +41,21 @@ var Grid = (function(){
 			cell.x = i % this.w;
 			cell.i = i;
 			cell.contents = {};
-		}
+		}	
+		this.createShip();
 	};
+	
+	// create player ship
+	api.createShip = function(){
+	
+		var ship = new PlayerCellObj();
+		this.cells[0].contents = ship;
+		
+	}
 	
 	// initial setup
 	api.setup();
-	
-	// return public api to Grid
+	// return public api to Game
 	return api;
-
 }());
-
-console.log(Grid);
+console.log(Game);
